cd1d34c9fff2f7326ec026960883af1163fefd36
Mohammed S Fadin <imohammad@me.com>
2014-12-09 00:09:19 +0300

    Parse User aboutme

--- test.js
+++ test.js
@@ -2,11 +2,12 @@
 
 var coderwall = require('./lib/cwall-node.js');
 
-coderwall.getUserInfo('mohammedfadin', function (data) {
+coderwall.getUserInfo('mohammedfadin', function(data) {
     console.log(data);
-    require('http').createServer(function (req, res) {
+    // Uncomment below if not needed
+    require('http').createServer(function(req, res) {
         res.end(JSON.stringify(data));
-    }).listen('3000', function () {
-        console.log('Check on port 3000 for result');
+    }).listen('3000', function() {
+        console.log('Check results on port 3000 for result');
     });
 });
